8812435de56224edb1b1a66ee1ecad9bf1dea3f1
julius-tamale <juliotamex@proton.me>
2022-12-09 20:46:23 +0200

    js update

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function increment() {
     count++;
     document.getElementById('count-el').innerText = count;
 }
+function saveEl() {
+    let save = document.getElementById('entries');
+    save.textContent += count + ' - ';
+}
